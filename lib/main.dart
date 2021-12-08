c15f9388f67af1bfa5799ff18663dff90ba7c02a
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-12-08 08:04:44 -0300

    aula(57): colunas e linhas

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Frases do dia",
-      home: Container(
-        color: Colors.white,
+      home: Row(
+        children: const [
+          Text(" T1 "),
+          Text(" T2 "),
+          Text(" T3 "),
+        ],
       )));
 }
