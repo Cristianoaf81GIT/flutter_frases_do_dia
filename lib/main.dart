40f24f67bebc1678eb95857fe7062291fa0cdbd1
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-12-23 19:45:21 -0300

    aula(58): formatação de texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit
 void main() {
   runApp(MaterialApp(
       title: "Frases do dia",
-      home: Row(
-        children: const [
-          Text(" T1 "),
-          Text(" T2 "),
-          Text(" T3 "),
-        ],
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          children: const <Widget>[
+            Text(
+              '''
+
+Lorem ipsum ''',
+              style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.black,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 0,
+                  wordSpacing: 0,
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.solid,
+                  decorationColor: Colors.greenAccent),
+            )
+          ],
+        ),
       )));
 }
