c683df16817acc285c341be4b18a459eb53c6064
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-01-13 19:02:53 -0300

    aula(60): espaçamentos

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,27 @@ import 'package:flutter/material.dart';
 // Lorem ipsum dolor sit amet, consectetur adipiscing elit
 void main() {
   runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Frases do dia",
       home: Container(
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            TextButton(
-                onPressed: () {
-                  print("botão pressionado");
-                },
-                child: Text(
-                  "Botão",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      decoration: TextDecoration.none),
-                ))
+        // color: Colors.white,
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        // margin: EdgeInsets.only(top: 50, left: 50),
+        margin: EdgeInsets.all(20),
+        decoration:
+            BoxDecoration(border: Border.all(width: 3, color: Colors.white)),
+        child: Row(
+          children: const <Widget>[
+            Text("t1"),
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: Text("t2"),
+            ),
+            Text("t3")
+            // Text(
+            //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id.",
+            //   textAlign: TextAlign.justify,
+            // )
           ],
         ),
       )));
