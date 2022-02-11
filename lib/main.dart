4d897cfeb19a9d96a1e3237f16d732c4871090a1
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-02-11 07:28:41 -0300

    aula(61): alinhamentos

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,13 @@ void main() {
       debugShowCheckedModeBanner: true,
       title: "Frases do dia",
       home: Container(
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        // margin: EdgeInsets.fromLTRB(10, 20, 30, 40),
-        // margin: EdgeInsets.only(top: 50, left: 50),
-        margin: EdgeInsets.all(0),
+        margin: EdgeInsets.only(top: 40),
         decoration:
             BoxDecoration(border: Border.all(width: 3, color: Colors.blue)),
         child: Row(
-          children: const <Widget>[
-            Text("T1"),
-
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Text("T2"),
-            ),
-
-            Text("T3")
-            // Text(
-            //   "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-            //   textAlign: TextAlign.justify,
-            // )
-          ],
+          children: const <Widget>[Text("T1"), Text("T2"), Text("T3")],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
         ),
       )));
 }
