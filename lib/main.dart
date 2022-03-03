bdc079fbbe14bfcd5fe554455d01697208ba8876
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-03-03 13:14:49 -0300

    aula(63): Layout com scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,25 @@ import 'package:flutter/material.dart';
 // Lorem ipsum dolor sit amet, consectetur adipiscing elit
 void main() {
   runApp(MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: "Frases do dia",
-      home: Container(
-        margin: EdgeInsets.only(top: 40),
-        decoration:
-            BoxDecoration(border: Border.all(width: 3, color: Colors.blue)),
-        /**
-             * contain, cover, fill, fitHeight, fitWidth, none, scaleDown
-             */
-        child: Image.asset(
-          "images/mesa.jpg",
-          fit: BoxFit.scaleDown,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Instagram"),
+          backgroundColor: Colors.green,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Text("Conteúdo principal"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.lightGreen,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [Text("Texto 1"), Text("Texto 2")],
+            ),
+          ),
         ),
       )));
 }
